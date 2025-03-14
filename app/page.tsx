91cb02dad4ef8d237853fa507fdb2351410c8e56
Adam Hammad <mail@adamham.dev>
2025-03-14 17:05:04 +0100

    updated resume

--- app/page.tsx
+++ app/page.tsx
@@ -159,7 +159,7 @@ export default function Page() {
 							<Card key={work.company}>
 								<CardHeader>
 									<div className='flex items-center justify-between gap-x-2 text-base'>
-										<h3 className='inline-flex items-center justify-center gap-x-1 font-semibold leading-none'>
+										<h3 className='inline-flex justify-center gap-x-1 gap-y-1 font-semibold flex-col'>
 											<a
 												className='hover:underline'
 												href={work.link}
@@ -167,8 +167,16 @@ export default function Page() {
 											>
 												{work.company}
 											</a>
+										</h3>
+										<div className='text-sm tabular-nums text-gray-500'>
+											{work.start} - {work.end}
+										</div>
+									</div>
 
-											<span className='inline-flex gap-x-1'>
+									<h4 className='font-mono text-sm leading-none'>
+										{work.title}
+									</h4>
+									<span className='inline-flex gap-x-1'>
 												{work.badges.map((badge) => (
 													<Badge
 														variant='secondary'
@@ -179,15 +187,6 @@ export default function Page() {
 													</Badge>
 												))}
 											</span>
-										</h3>
-										<div className='text-sm tabular-nums text-gray-500'>
-											{work.start} - {work.end}
-										</div>
-									</div>
-
-									<h4 className='font-mono text-sm leading-none'>
-										{work.title}
-									</h4>
 								</CardHeader>
 								<CardContent className='mt-2 text-xs'>
 									{work.description}
